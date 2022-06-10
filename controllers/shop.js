8f53fe3dc6de885e4ba33536c4d3d5826ea944e8
Antoan Petrov <antoan999_kn@abv.bg>
2022-06-10 22:34:47 +0300

    Send a invoice as a stream

--- controllers/shop.js
+++ controllers/shop.js
@@ -187,14 +187,18 @@ exports.getInvoice = (req, res, next) => {
 
             const invoiceName = 'invoice-' + orderId + '.pdf';
             const invoicePath = path.join('data', 'invoices', invoiceName); //data folder, invoice folder and invoice name
-            fs.readFile(invoicePath, (err, data) => {
-                if (err) {
-                    return next(err);
-                }
-                res.setHeader('Content-Type', 'application/pdf'); //open pdf file
-                res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"'); //attachment -> download file // inline -> open file
-                res.send(data);
-            });
+            // fs.readFile(invoicePath, (err, data) => {
+            //     if (err) {
+            //         return next(err);
+            //     }
+            //     res.setHeader('Content-Type', 'application/pdf'); //open pdf file
+            //     res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"'); //attachment -> download file // inline -> open file
+            //     res.send(data);
+            // });
+            const file = fs.createReadStream(invoicePath);
+            res.setHeader('Content-Type', 'application/pdf');
+            res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
+            file.pipe(res); //pipe file stream into a response writable stream
         })
         .catch(err => next(err));
 };
